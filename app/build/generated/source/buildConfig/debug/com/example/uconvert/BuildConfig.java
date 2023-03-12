563875ac9e85b43fd6dc4652193048232792f04e
Abhishek  kandari <abhishekkandari96@gmail.com>
2023-03-12 12:54:08 +0530

    Initial commit

---
+++ app/build/generated/source/buildConfig/debug/com/example/uconvert/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.uconvert;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.uconvert";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
